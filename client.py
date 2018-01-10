7ab36dd4f4bafb03d55613a2e2898c827e8a68ec
kostyk <rimshakonstantin@ya.ru>
2018-01-10 17:20:10 +0500

    added new functionals

--- client.py
+++ client.py
@@ -1,17 +1,26 @@
+# Программа клиента, запрашивающего текущее время
+
 import sys
 import argparse
+from socket import *
 
 def createParser ():
     parser = argparse.ArgumentParser()
-    parser.add_argument('ip', help='ip server')
-    parser.add_argument('port', type=int, help='port server', default=7777)
+    parser.add_argument('addr', help='Server ip address')
+    parser.add_argument('port', nargs='?', type=int, help='Server port', default=7777)
 
     return parser
 
 
 if __name__ == '__main__':
     parser = createParser()
-    namespace = parser.parse_args(sys.argv[1:])
+    arguments = parser.parse_args(sys.argv[1:])
 
-    print("ip: {}".format(namespace.ip))
-    print("port: {}".format(namespace.port))
+    print("Try to connect:")
+    print("ip: {}".format(arguments.addr))
+    print("port: {}".format(arguments.port))
+    s = socket(AF_INET, SOCK_STREAM)  # Создать сокет TCP
+    s.connect((arguments.addr, arguments.port))   # Соединиться с сервером
+    tm = s.recv(1024)                # Принять не более 1024 байтов данных
+    s.close()
+    print("Текущее время: %s" % tm.decode('ascii'))
--- server.py
+++ server.py
@@ -1,17 +1,38 @@
+# Программа сервера времени
+
 import sys
 import argparse
+from socket import *
+import time
 
 def createParser ():
     parser = argparse.ArgumentParser()
-    parser.add_argument('ip', help='Server ip listening')
-    parser.add_argument('port', type=int, help='Server port', default=7777)
+    parser.add_argument('-a', '--addr', nargs='?', help='Server ip listening', default='')
+    parser.add_argument('-p', '--port', nargs='?', type=int, help='Server port', default=7777)
 
     return parser
 
 
 if __name__ == '__main__':
     parser = createParser()
-    namespace = parser.parse_args(sys.argv[1:])
+    arguments = parser.parse_args(sys.argv[1:])
 
-    print("ip: {}".format(namespace.ip))
-    print("port: {}".format(namespace.port))
+    print("Server running on:")
+    print("ip: {}".format(arguments.addr))
+    print("port: {}".format(arguments.port))
+    s = socket(AF_INET, SOCK_STREAM)  # Создает сокет TCP
+
+    s.bind((arguments.addr, arguments.port))                # Присваивает порт 8888
+    s.listen(5)                       # Переходит в режим ожидания запросов;
+                                      # одновременно обслуживает не более
+                                      # 5 запросов.
+    while True:
+        client, addr = s.accept()     # Принять запрос на соединение
+        print("Получен запрос на соединение от %s" % str(addr))
+        
+        timestr = time.ctime(time.time()) + "\n"
+        
+        # Обратите внимание, дальнейшая работа ведётся с сокетом клиента
+        client.send(timestr.encode('ascii'))   # <- По сети должны передаваться байты,
+                                               # поэтому выполняется кодирование строки 
+        client.close()
